ffb5bd7ff84417a33197ae1945e4f01c911a9284
josellia <joseliaapires@hotmail.com>
2022-01-16 13:28:23 -0300

    test: teste da property label do input

--- src/app/shared/components/inputs/input/input.component.spec.ts
+++ src/app/shared/components/inputs/input/input.component.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {  ComponentFixture, TestBed } from "@angular/core/testing";
+import { Component,  DebugElement } from '@angular/core';
+import { InputComponent } from "./input.component";
+import { By } from "@angular/platform-browser";
+import { InputPageComponent } from "src/app/pages/input-page/input-page.component";
+import { FormGroup, FormsModule } from "@angular/forms";
 
-import { InputComponent } from './input.component';
+@Component({
+  template: `
+  <div [formGroup]="formGroup">
+  <app-input
+  label="Cpf"
+></app-input>
+  </div>
+  `
+})
+class FakeComponet {
+  formGroup!: FormGroup;
+  label = '' ;
+}
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let fake: FakeComponet;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations:[InputComponent, FakeComponet]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputComponent);
@@ -22,4 +39,29 @@ describe('InputComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should correctly render the passed @Input label', () => {
+  component.label = 'Cpf';
+  fixture.detectChanges();
+  expect(fixture.nativeElement.textContent).toBe('Cpf')
+  });
+
+  it('should other form test @Input label', () => {
+
+  })
 });
+
+// it('should name in input', () => {
+//   fixture.detectChanges();
+
+//   const inEl = fixture.debugElement.query(By.css('input'));
+//   expect(inEl.nativeElement.type).toBe('text');
+// })
+
+/*
+Fixture: recebe o tipo  ComponentFixture que é um equipamento de teste para interagir com o 
+componente criado e seu elemento corresponde.
+
+DebugElement: é uma classe do angular que contém referencias e métodos relevantes para
+investigar um elemento ou componente
+ */
